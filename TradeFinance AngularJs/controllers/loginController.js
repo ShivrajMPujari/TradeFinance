55becb7d55722394efcaafb92cc786f2ce6f55ed
bridgelabz <shivrajpujari62@gmail.com>
2018-08-14 10:25:21 +0530

    commit

--- TradeFinance AngularJs/controllers/loginController.js
+++ TradeFinance AngularJs/controllers/loginController.js
@@ -1,39 +1,12 @@
 app.controller('loginController', function($scope, $state , $http ,$mdToast ,$document , $stateParams) {
 
-
-  //show toast on login failure
-  $scope.showToastAccountVerify = function() {
-     $mdToast.show (
-        $mdToast.simple()
-        .textContent('Check your email to verify your account')
-        .hideDelay(3000)
-     );
-   }
-
-
-   $scope.showToastForResetSuccess = function() {
-      $mdToast.show (
-         $mdToast.simple()
-         .textContent('Your has been reset sucessfully ..')
-         .hideDelay(3500)
-      );
-    }
-
-   $scope.showToastAccountExist = function() {
-      $mdToast.show (
-         $mdToast.simple()
-         .textContent('you are an existing user')
-         .hideDelay(3000)
-      );
-    }
-
-   $scope.showToast = function() {
-      $mdToast.show (
-         $mdToast.simple()
-         .textContent('Login failed ..your email or password is wrong')
-         .hideDelay(3000)
-      );
-    }
+    $scope.showToastCommon = function(message) {
+       $mdToast.show (
+          $mdToast.simple()
+          .textContent(message)
+          .hideDelay(4000)
+       );
+     }
 
    $scope.loginFailed=false;
     $scope.submitLoginForm = function() {
@@ -50,10 +23,12 @@ app.controller('loginController', function($scope, $state , $http ,$mdToast ,$do
         $http.post(loginUrl,formData).then(
           function(response){
             console.log(response);
+            $scope.showToastCommon(response.data.message);
             $state.go('home');
           },
           function(reason){
-            console.log(reason);
+            console.log(reason.data.message);
+              $scope.showToastCommon(reason.data.message);
           $scope.loginFailed=true;
           }
         );
@@ -63,7 +38,7 @@ app.controller('loginController', function($scope, $state , $http ,$mdToast ,$do
       $state.go('register');
     }
 
-    var rolesArray =['Importer','Exporter','Customer'];
+    var rolesArray =['Importer','Exporter','Custom','ImporterBank','ExporterBank'];
     $scope.roles=rolesArray;
 
     $scope.confirmPassword=false;
@@ -88,10 +63,10 @@ app.controller('loginController', function($scope, $state , $http ,$mdToast ,$do
         $http.post(registerUrl,registerFormData).then( function(response){
           console.log(response);
           $state.go('login');
-          $scope.showToastAccountVerify();
+          $scope.showToastCommon(response.data.message);
         } ,function(reason){
           console.log(reason);
-          $scope.showToastAccountExist();
+          $scope.showToastCommon(reason.data.message);
         } );
       }else{
         $scope.confirmPassword=true;
@@ -109,39 +84,62 @@ app.controller('loginController', function($scope, $state , $http ,$mdToast ,$do
     }
 
 
-      $scope.resetFormPasswordMsg=false;
-    $scope.submitResetPassword = function(){
-
-      if($scope.resetFormPassword==$scope.resetFormConfirmPassword){
-
-        console.log($stateParams.id);
-        var resetPasswordData = {
-
-          newPassword:$scope.resetFormPassword,
-          uuid:$stateParams.id
-
-        };
-        var resetPasswordUrl ='http://localhost:8080/user/resetPassword';
-        $http.post(resetPasswordUrl,resetPasswordData).then(
-          function(response){
-            console.log(response);
-            $state.go('login');
-            $scope.showToastForResetSuccess();
-          }
-          ,function(reason){
-            console.log(reason);
-            $state.go('sessionOut');
-          });
 
 
-      }else{
-        $scope.resetFormPasswordMsg=true;
+    $scope.forgotPassword= function (){
+
+      var data ={
+        email:$scope.emailForForgotPassword
       }
 
+      var forgotPasswordUrl = 'http://localhost:8080/user/forgotPassword';
 
-
+      $http.post(forgotPasswordUrl,data).then(function(response){
+        console.log(response);
+        if(response.data.code==200){
+            $scope.showToastCommon(response.data.message);
+            $state.go('login');
+        }
+      },
+      function(reason){
+        console.log(reason);
+        $scope.showToastCommon(reason.data.message);
+      }
+    );
 
     }
+
+  $scope.resetFormPasswordMsg=false;
+  $scope.submitResetPassword = function(){
+
+    if($scope.resetFormPassword==$scope.resetFormConfirmPassword){
+
+      console.log($stateParams.id);
+      var resetPasswordData = {
+
+        newPassword:$scope.resetFormPassword,
+        uuid:$stateParams.id
+
+      }
+      var resetPasswordUrl ='http://localhost:8080/user/resetPassword';
+      $http.post(resetPasswordUrl,resetPasswordData).then(
+        function(response){
+          console.log(response);
+          $state.go('login');
+          $scope.showToastCommon(response.data.message);
+        }
+        ,function(reason){
+          console.log(reason);
+          $state.go('sessionOut');
+            $scope.showToastCommon(reason.data.message);
+        });
+
+
+    }else{
+      $scope.resetFormPasswordMsg=true;
+    }
+
+  }
 
 
 
